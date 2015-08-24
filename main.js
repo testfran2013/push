9606ef40f1ac3004145844c408d6fc78e2d87190
testfran2013 <testfran2013@gmail.com>
2015-08-24 15:48:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -213,7 +213,7 @@ window.addEventListener('load', function() {
   // Check that service workers are supported, if so, progressively
   // enhance and add push messaging support, otherwise continue without it.
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
     .then(function(reg){
 	alert("reg =="+reg);
     }, function(err){
